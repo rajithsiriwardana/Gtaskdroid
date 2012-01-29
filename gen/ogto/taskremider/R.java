cca745c106082a5cde4bbe847032c3f992abcb50
Rajith <rajith@ubuntu.ubuntu-domain>
2012-01-29 22:22:47 +0530

    bug in the program need a review

--- gen/ogto/taskremider/R.java
+++ gen/ogto/taskremider/R.java
@@ -12,12 +12,36 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_menu_add=0x7f020001;
+    }
+    public static final class id {
+        public static final int body=0x7f060001;
+        public static final int comfirm=0x7f060004;
+        public static final int menu_delete=0x7f060007;
+        public static final int menu_insert=0x7f060006;
+        public static final int reminder_date=0x7f060002;
+        public static final int reminder_time=0x7f060003;
+        public static final int text1=0x7f060005;
+        public static final int title=0x7f060000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int reminder_edit=0x7f030000;
+        public static final int reminder_list=0x7f030001;
+        public static final int reminder_row=0x7f030002;
+    }
+    public static final class menu {
+        public static final int list_menu=0x7f050000;
+        public static final int list_menu_item_longpress=0x7f050001;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
+        public static final int body=0x7f040006;
+        public static final int date=0x7f040002;
+        public static final int menu_delete=0x7f040008;
+        public static final int menu_insert=0x7f040007;
+        public static final int no_reminders=0x7f040001;
+        public static final int save=0x7f040005;
+        public static final int time=0x7f040003;
+        public static final int title=0x7f040004;
     }
 }
--- src/ogto/taskremider/ReminderListActivity.java
+++ src/ogto/taskremider/ReminderListActivity.java
@@ -1,13 +1,105 @@
 package ogto.taskremider;
 
-import android.app.Activity;
-import android.os.Bundle;
 
-public class ReminderListActivity extends Activity {
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ReminderListActivity extends ListActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.reminder_list);
+        
+        addTestData();					//adding data till the implementation of the data base
+        registerForContextMenu(getListView());
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	// TODO Auto-generated method stub
+    	super.onCreateOptionsMenu(menu);
+    	MenuInflater mi=getMenuInflater();
+    	mi.inflate(R.menu.list_menu, menu);
+    	return true;    	
+    }
+   
+    
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+    	switch (item.getItemId()) {
+		case R.id.menu_insert:
+			
+			createReminder();
+			return true;
+		}
+    	return super.onMenuItemSelected(featureId, item);
+    }
+    
+  
+
+	@Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+    		ContextMenuInfo menuInfo) {
+    	// TODO Auto-generated method stub
+    	super.onCreateContextMenu(menu, v, menuInfo);
+    	
+    	MenuInflater mi=getMenuInflater();
+    	mi.inflate(R.menu.list_menu_item_longpress, menu);
+    }
+	
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_delete:
+			//delete task
+			return true;
+			
+		}
+		return super.onContextItemSelected(item);
+	}
+    
+    
+	private void addTestData() {
+		String [] items= new String[] {"foo","bar","fizz","bin"};
+        
+        ArrayAdapter <String> adapter=new ArrayAdapter <String> (this,R.layout.reminder_row,R.id.text1,items);
+        setListAdapter(adapter);
+	}
+    
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {      //context menu on long click
+    	// TODO Auto-generated method stub
+    	super.onListItemClick(l, v, position, id);
+    	Intent intent=new Intent(this,ReminderEditActivity.class);
+    	intent.putExtra("RowId", id);
+    	startActivity(intent);
+    	
+    }
+    
+    private static final int ACTIVITY_CREATE=0;
+    private void createReminder() {
+		Intent intent=new Intent(this, ReminderEditActivity.class);
+		startActivityForResult(intent, ACTIVITY_CREATE);
+		
+	}
+    
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	// TODO Auto-generated method stub
+    	super.onActivityResult(requestCode, resultCode, data);
+    	
+    	//reload the list here
     }
 }
