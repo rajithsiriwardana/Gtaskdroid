f41600923dd5ae86923b93ef89243a427154d0ee
Rajith <rajith@ubuntu.ubuntu-domain>
2012-02-02 22:16:59 +0530

    testing version currently developing the GUI part

--- gen/ogto/taskremider/R.java
+++ gen/ogto/taskremider/R.java
@@ -17,30 +17,30 @@ public final class R {
     }
     public static final class id {
         public static final int Button01=0x7f07000e;
-        public static final int add_reminder_checkbox=0x7f070010;
-        public static final int all_day_checkbox=0x7f070011;
+        public static final int add_reminder_checkbox=0x7f070012;
+        public static final int all_day_checkbox=0x7f070010;
         public static final int cancel=0x7f070000;
         public static final int checkBox1=0x7f07000c;
         public static final int event_end_date=0x7f07000a;
         public static final int event_end_time=0x7f07000f;
         public static final int event_start_date=0x7f070009;
         public static final int event_start_time=0x7f07000d;
-        public static final int location=0x7f070006;
+        public static final int location=0x7f070007;
         public static final int menu_delete=0x7f070018;
         public static final int menu_insert=0x7f070016;
         public static final int menu_settings=0x7f070017;
-        public static final int note=0x7f070004;
+        public static final int note=0x7f070005;
         public static final int reminder_date=0x7f070014;
         public static final int reminder_time=0x7f070013;
-        public static final int save=0x7f070008;
+        public static final int save=0x7f070001;
         public static final int text1=0x7f070015;
-        public static final int textView1=0x7f070002;
-        public static final int textView2=0x7f070012;
+        public static final int textView1=0x7f070003;
+        public static final int textView2=0x7f070011;
         public static final int textView3=0x7f07000b;
-        public static final int textView4=0x7f070003;
-        public static final int textView5=0x7f070005;
-        public static final int textView6=0x7f070007;
-        public static final int title=0x7f070001;
+        public static final int textView4=0x7f070004;
+        public static final int textView5=0x7f070006;
+        public static final int textView6=0x7f070008;
+        public static final int title=0x7f070002;
     }
     public static final class layout {
         public static final int event_edit=0x7f030000;
