ed26ccfd54ed0e405b1f786bb767d6f0f825c50e
Rajith <rajith@ubuntu.ubuntu-domain>
2012-02-02 08:50:05 +0530

    developing GUIs, changes are done to adhere to project requirements

--- gen/ogto/taskremider/R.java
+++ gen/ogto/taskremider/R.java
@@ -16,53 +16,76 @@ public final class R {
         public static final int ic_menu_preferences=0x7f020002;
     }
     public static final class id {
-        public static final int menu_delete=0x7f070008;
-        public static final int menu_insert=0x7f070006;
-        public static final int menu_settings=0x7f070007;
-        public static final int note=0x7f070001;
-        public static final int reminder_date=0x7f070002;
-        public static final int reminder_time=0x7f070003;
-        public static final int save=0x7f070004;
-        public static final int text1=0x7f070005;
-        public static final int title=0x7f070000;
+        public static final int Button01=0x7f07000e;
+        public static final int add_reminder_checkbox=0x7f070010;
+        public static final int all_day_checkbox=0x7f070011;
+        public static final int cancel=0x7f070000;
+        public static final int checkBox1=0x7f07000c;
+        public static final int event_end_date=0x7f07000a;
+        public static final int event_end_time=0x7f07000f;
+        public static final int event_start_date=0x7f070009;
+        public static final int event_start_time=0x7f07000d;
+        public static final int location=0x7f070006;
+        public static final int menu_delete=0x7f070018;
+        public static final int menu_insert=0x7f070016;
+        public static final int menu_settings=0x7f070017;
+        public static final int note=0x7f070004;
+        public static final int reminder_date=0x7f070014;
+        public static final int reminder_time=0x7f070013;
+        public static final int save=0x7f070008;
+        public static final int text1=0x7f070015;
+        public static final int textView1=0x7f070002;
+        public static final int textView2=0x7f070012;
+        public static final int textView3=0x7f07000b;
+        public static final int textView4=0x7f070003;
+        public static final int textView5=0x7f070005;
+        public static final int textView6=0x7f070007;
+        public static final int title=0x7f070001;
     }
     public static final class layout {
-        public static final int reminder_edit=0x7f030000;
-        public static final int reminder_list=0x7f030001;
-        public static final int reminder_row=0x7f030002;
+        public static final int event_edit=0x7f030000;
+        public static final int reminder_edit=0x7f030001;
+        public static final int reminder_list=0x7f030002;
+        public static final int reminder_row=0x7f030003;
     }
     public static final class menu {
         public static final int list_menu=0x7f060000;
         public static final int list_menu_item_longpress=0x7f060001;
     }
     public static final class string {
+        public static final int add_reminder=0x7f05001b;
+        public static final int all_day=0x7f05001c;
         public static final int app_name=0x7f050000;
-        public static final int date=0x7f050002;
-        public static final int menu_delete=0x7f050008;
-        public static final int menu_insert=0x7f050007;
-        public static final int menu_settings=0x7f05001a;
+        public static final int cancel=0x7f05001a;
+        public static final int date=0x7f050020;
+        public static final int duration=0x7f05001d;
+        public static final int duration_to=0x7f05001e;
+        public static final int event_location=0x7f05001f;
+        public static final int menu_delete=0x7f050007;
+        public static final int menu_insert=0x7f050006;
+        public static final int menu_settings=0x7f050019;
         public static final int no_reminders=0x7f050001;
-        public static final int note=0x7f050006;
-        public static final int notify_new_task_msg=0x7f05000a;
-        public static final int notify_new_task_title=0x7f05000b;
-        public static final int pref_category_datetime_key=0x7f050013;
-        public static final int pref_category_datetime_title=0x7f050014;
-        public static final int pref_category_task_default_key=0x7f05000c;
-        public static final int pref_category_task_default_title=0x7f05000d;
-        public static final int pref_default_time_from_now_dialog_title=0x7f050016;
-        public static final int pref_default_time_from_now_key=0x7f050015;
-        public static final int pref_default_time_from_now_message=0x7f050017;
-        public static final int pref_default_time_from_now_summary=0x7f050018;
-        public static final int pref_default_time_from_now_title=0x7f050019;
-        public static final int pref_task_title_dialog_title=0x7f05000f;
-        public static final int pref_task_title_key=0x7f05000e;
-        public static final int pref_task_title_message=0x7f050010;
-        public static final int pref_task_title_summary=0x7f050011;
-        public static final int pref_task_title_title=0x7f050012;
-        public static final int save=0x7f050005;
-        public static final int task_saved_message=0x7f050009;
-        public static final int time=0x7f050003;
-        public static final int title=0x7f050004;
+        public static final int note=0x7f050005;
+        public static final int notify_new_task_msg=0x7f050009;
+        public static final int notify_new_task_title=0x7f05000a;
+        public static final int pref_category_datetime_key=0x7f050012;
+        public static final int pref_category_datetime_title=0x7f050013;
+        public static final int pref_category_task_default_key=0x7f05000b;
+        public static final int pref_category_task_default_title=0x7f05000c;
+        public static final int pref_default_time_from_now_dialog_title=0x7f050015;
+        public static final int pref_default_time_from_now_key=0x7f050014;
+        public static final int pref_default_time_from_now_message=0x7f050016;
+        public static final int pref_default_time_from_now_summary=0x7f050017;
+        public static final int pref_default_time_from_now_title=0x7f050018;
+        public static final int pref_task_title_dialog_title=0x7f05000e;
+        public static final int pref_task_title_key=0x7f05000d;
+        public static final int pref_task_title_message=0x7f05000f;
+        public static final int pref_task_title_summary=0x7f050010;
+        public static final int pref_task_title_title=0x7f050011;
+        public static final int save=0x7f050004;
+        public static final int task_saved_message=0x7f050008;
+        public static final int time=0x7f050002;
+        public static final int title=0x7f050003;
     }
     public static final class xml {
         public static final int task_preferences=0x7f040000;
