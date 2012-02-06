30023a048e32171d2c92aa89ed1edf424755dbfe
Rajith <rajith@ubuntu.ubuntu-domain>
2012-02-06 19:35:06 +0530

    success 20% completed in taskHandler

---
+++ src/ogto/integration/OnAlarmReceiver.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package ogto.integration;
+
+import ogto.dataaccess.RemindersDbAdapter;
+import ogto.logic.ReminderService;
+import ogto.logic.WakeReminderIntentService;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * @author rajith
+ *
+ */
+public class OnAlarmReceiver extends BroadcastReceiver {
+
+	/* (non-Javadoc)
+	 * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
+	 */
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		long rowId=intent.getExtras().getLong(RemindersDbAdapter.KEY_ROWID);
+		WakeReminderIntentService.acquireStaticLock(context);
+		Intent i=new Intent(context, ReminderService.class);
+		i.putExtra(RemindersDbAdapter.KEY_ROWID, rowId);
+		context.startService(i);
+	}
+
+}
---
+++ src/ogto/logic/ReminderManager.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package ogto.logic;
+
+import java.util.Calendar;
+
+import ogto.dataaccess.RemindersDbAdapter;
+import ogto.integration.OnAlarmReceiver;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * @author rajith
+ *
+ */
+public class ReminderManager {
+	
+	private Context mContext;
+	private AlarmManager mAlarmManager;
+	
+	public ReminderManager(Context context) {
+		mContext=context;
+		mAlarmManager=(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+	}
+	
+	public void setReminder(long taskId, Calendar when) {
+		
+		Intent intent=new Intent(mContext, OnAlarmReceiver.class);
+		intent.putExtra(RemindersDbAdapter.KEY_ROWID, (long)taskId);
+		
+		PendingIntent pIntent=PendingIntent.getBroadcast(mContext, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+		
+		mAlarmManager.set(AlarmManager.RTC_WAKEUP, when.getTimeInMillis(), pIntent);		
+		
+	}
+
+}
