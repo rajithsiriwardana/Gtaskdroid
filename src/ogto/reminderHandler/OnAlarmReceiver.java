7350bb4be131b74daa1fe914fb70678e6cde792e
Rajith <rajith@ubuntu.ubuntu-domain>
2012-02-16 07:46:35 +0530

    evaluating verion 0.02

---
+++ src/ogto/reminderHandler/OnAlarmReceiver.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package ogto.reminderHandler;
+
+import ogto.dataaccess.EventsDbAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * @author rajith
+ *
+ */
+public class OnAlarmReceiver extends BroadcastReceiver {
+
+	/* (non-Javadoc)
+	 * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
+	 */
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		long rowId=intent.getExtras().getLong(EventsDbAdapter.KEY_ROWID);
+		WakeReminderIntentService.acquireStaticLock(context);
+		Intent i=new Intent(context, ReminderService.class);
+		i.putExtra(EventsDbAdapter.KEY_ROWID, rowId);
+		context.startService(i);
+	}
+
+}
